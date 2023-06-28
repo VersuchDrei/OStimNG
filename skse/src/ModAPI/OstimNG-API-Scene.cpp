17e9880cd0de4880070881bba01e300a229243c2
Monitor144hz <monitor.144.hz.100.srgb@gmail.com>
2023-06-28 21:47:08 +0100

    initial scene api, non-functional for now

---
+++ skse/src/ModAPI/OstimNG-API-Scene.cpp
@@ -0,0 +1,22 @@
+#include "OstimNG-API-Scene.h"
+OstimNG_API::Scene::ISceneInterface* g_ostimSceneInterface = nullptr;
+namespace OstimNG_API::Scene
+{
+     ///
+    /// Gets pointer to API singleton from external plugin.
+    ///
+    ISceneInterface* OstimNG_API::Scene::ISceneInterface::GetAPI() 
+    {
+        if (g_ostimSceneInterface) return g_ostimSceneInterface;    
+
+        const auto pluginHandle = GetModuleHandle(L"Ostim.dll");
+        const auto requestAPIFunction = reinterpret_cast<_RequestPluginAPI_Scene>(GetProcAddress(pluginHandle, "RequestPluginAPI_Conditions"));
+         if (!requestAPIFunction) {
+            return nullptr;
+        }
+        const auto plugin = SKSE::PluginDeclaration::GetSingleton();
+        g_ostimSceneInterface = requestAPIFunction(InterfaceVersion::V1, std::string(plugin->GetName()).c_str(), plugin->GetVersion());
+        return g_ostimSceneInterface; 
+    }
+}
+
